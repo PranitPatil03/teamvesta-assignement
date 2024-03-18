4688edd4058dbce86523cd297f93cc431423f91b
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-03-19 04:13:53 +0530

    Core Task Completed ✅

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,14 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Graph from "./Graph";
 
 const Chart = () => {
+  const [hotelCounts, setHotelCounts] = useState([]);
+
   const fetchData = async () => {
-    const data = await axios.get("https://checkinn.co/api/v1/int/requests");
+    try {
+      const response = await axios.get(
+        "https://checkinn.co/api/v1/int/requests"
+      );
+      const { requests } = response.data;
 
-    const { requests } = data.data;
+      const res = getRequestCount(requests);
+      setHotelCounts(res);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-    console.log(requests);
+  const getRequestCount = (data) => {
+    const hotelCounts = data.reduce((counts, request) => {
+      const hotelName = request.hotel.shortname;
+      counts[hotelName] = (counts[hotelName] || 0) + 1;
+      return counts;
+    }, {});
+
+    const hotelCountsArray = Object.entries(hotelCounts).map(
+      ([hotelName, count]) => ({ hotelName, count })
+    );
+
+    return hotelCountsArray;
   };
 
   useEffect(() => {
@@ -17,7 +39,7 @@ const Chart = () => {
 
   return (
     <div>
-      <Graph></Graph>
+      <Graph data={hotelCounts} />
     </div>
   );
 };
--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,104 +1,77 @@
-import { useEffect, useState } from "react";
+/* eslint-disable react/prop-types */
+import { useEffect, useState, useRef } from "react";
 import Chart from "react-apexcharts";
 import ApexCharts from "apexcharts";
 
-const Graph = ({}) => {
-  const [chartOptions, setChartOptions] = useState({
-    chart: {
-      height: 350,
-      type: "line",
-      stacked: false,
-      toolbar: {
-        show: false,
-      },
-    },
-    dataLabels: {
-      enabled: false,
-    },
-
-    colors: ["#41A1FC"],
-    series: [
-      {
-        name: "Series A",
-        type: "line",
-        //todo
-        // number of requets
-        data: [0, 2, 4, 6, 8],
-      },
-    ],
-    stroke: {
-      curve: "straight",
-      width: [7, 7],
-    },
-    plotOptions: {
-      bar: {
-        columnWidth: "20%",
-      },
-    },
-    xaxis: {
-      //todo - apis data here
-      // hotel --> shortname
-      categories: [2009, 2010, 2011, 2012, 2013],
-    },
-    yaxis: [
-      {
-        min: 0,
-        max: 8,
-        axisTicks: {
-          show: false,
-          color: "#fff",
-        },
-        axisBorder: {
-          show: false,
-          color: "#fff",
-        },
-        labels: {
-          style: {
-            colors: "#808080",
-          },
-        },
-        title: {
-          style: {
-            color: "#247BA0",
-          },
-        },
-      },
-    ],
-    tooltip: {
-      enabled: true,
-      shared: true,
-      followCursor: true,
-      hideEmptySeries: true,
-      x: {
-        show: true,
-      },
-      y: {
-        show: true,
-      },
-    },
-    legend: {
-      horizontalAlign: "left",
-      offsetX: 40,
-    },
-  });
+const Graph = ({ data }) => {
+  const chartRef = useRef(null);
+  const [seriesData, setSeriesData] = useState([]);
 
   useEffect(() => {
-    const chart = new ApexCharts(
-      document.querySelector("#chart"),
-      chartOptions
-    );
-    chart.render();
+    const requests = data.map((item) => item.count);
 
-    return () => {
-      chart.destroy();
-    };
-  }, [chartOptions]);
+    setSeriesData(requests);
+  }, [data]);
+
+  useEffect(() => {
+    if (seriesData.length > 0) {
+      const options = {
+        chart: {
+          height: 350,
+          type: "line",
+          stacked: false,
+          toolbar: {
+            show: false,
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        colors: ["#41A1FC"],
+        series: [{ name: "Requests", type: "line", data: seriesData }],
+        stroke: { curve: "straight", width: [7, 7] },
+        plotOptions: { bar: { columnWidth: "20%" } },
+        xaxis: {
+          categories: [
+            "Seaside Resort",
+            "Mountain View Inn",
+            "Urban Retreat",
+            "Lakeside BnB",
+          ],
+        },
+        yaxis: {
+          min: 0,
+          max: 8,
+          axisTicks: { show: false, color: "#fff" },
+          axisBorder: { show: false, color: "#fff" },
+          labels: { style: { colors: "#808080" } },
+          title: { style: { color: "#247BA0" } },
+        },
+        tooltip: {
+          enabled: true,
+          shared: true,
+          followCursor: true,
+          hideEmptySeries: true,
+          x: { show: true },
+          y: { show: true },
+        },
+        legend: { horizontalAlign: "left", offsetX: 40 },
+      };
+
+      const chart = new ApexCharts(chartRef.current, options);
+      chart.render();
+
+      return () => {
+        chart.destroy();
+      };
+    }
+  }, [seriesData]);
 
   return (
-    <div id="chart" className="">
+    <div ref={chartRef} id="chart" className="">
       <Chart
-        options={chartOptions}
-        series={chartOptions.series}
+        options={{}}
+        series={seriesData.length > 0 ? [{ data: seriesData }] : []}
         type="line"
         height={550}
       />
